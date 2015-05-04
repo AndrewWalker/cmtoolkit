7ea4e66ed226e6c173221389e7909e3eb7205ead
Andrew Walker <walker.ab@gmail.com>
2015-05-04 23:24:56 +1000

    improving coverage

--- tests/test_region.py
+++ tests/test_region.py
@@ -6,8 +6,18 @@ class TestRegion(unittest.TestCase):
     def setUp(self):
         self.p = Circle(0.0, 1.0)
 
+    def test_empty_constructors(self):
+        r = Region()
+
     def test_onearg_constructors(self):
         r = Region(outer = self.p)
+
+    def test_list_constructors(self):
+        r = Region(outer = [self.p, self.p])
+
+    def test_bad_constructor(self):
+        with self.assertRaises(Exception):
+            r = Region(outer = [42])
 
     def test_interior_constructor(self):
         r = Region.interiorto(self.p)
